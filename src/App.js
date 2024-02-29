f61ced6f99102ab2760714067f37ce28f9e468dc
Juan Ignacio Canarsky <juancanarsky@gmail.com>
2024-02-29 18:18:15 -0300

    Segunda preentrega

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
+import ProfileCard from "./components/ProfileCard";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
-import CarritoCompras from "./imagenes/carrito de compras.ico"
+import CarritoCompras from "./imagenes/carritodecompras.ico"
 import CartWidget from "./components/CartWidget";
+import JoystickAzul from "./imagenes/azul 2.webp"
+import JoystickRojo from "./imagenes/rojo 2.webp"
+import JoystickDorado from "./imagenes/dorado 2.webp"
+import JoystickPlateado from "./imagenes/plateado 2.webp"
+
+import "bulma/css/bulma.css";
 
 function App(){
     return(
-            <div>
-                <NavBar brand="Electronic Sans" categoria1="Productos electrónicos" categoria2 ="Productos nuevos"/>
-                    <CartWidget icono={CarritoCompras} notificacion="123"/>
-                <ItemListContainer mensaje="Bienvenidos a Electronic Sans"/>
+        <div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <h1 className="title">Electronic Sans</h1>
+                </div>
+            </section>
+
+                        <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="columns is-4">
+                            <NavBar brand="Electronic Sans" categoria1="Productos electrónicos" categoria2 ="Productos nuevos"/>
+                        </div>
+                        <div className="columns is-4">
+                            <CartWidget icono={CarritoCompras} notificacion="123"/>
+                        </div>
+                        <div className="columns is-4">
+                            <ItemListContainer mensaje="Bienvenidos a Electronic Sans"/>
+                        </div>
+                    </div>
+                </section>
             </div>
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="columns is-3">
+                            <ProfileCard titulo="Joystick Dorado" arroba="@JoystickDorado" img={JoystickDorado}/>
+                        </div>
+                        <div className="columns is-3">
+                            <ProfileCard titulo="Joystick Plateado" arroba="@JoystickPlateado" img={JoystickPlateado}/>
+                        </div>
+                        <div className="columns is-3">
+                        <ProfileCard titulo="Joystick Azul" arroba="@JoystickAzul" img={JoystickAzul}/>
+                        </div>
+                        <div className="columns is-3">
+                        <ProfileCard titulo="Joystick Rojo" arroba="@JoystickRojo" img={JoystickRojo}/>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,9 +1,28 @@
+import {useState} from "react";
+
 function ProfileCard({titulo, arroba, img}){
+
+    function handleClick(){
+        setCount(count + 1);
+    }
+
+    const [count, setCount] = useState(0);
+
     return (
-        <div>
-            <img src={img}/>
-            <h3>{titulo}</h3>
-            <p>{arroba}</p>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={img} alt="imagen"/>
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <h3 className="title">{titulo}</h3>
+                    <p className="subtitle">{arroba}</p>
+                    <button onClick={handleClick} className="button is-succes is-fullwidth">+</button>
+                    <p>Camtodad: {count}</p>
+                </div>
+            </div>
         </div>
     )
 }
